86e4ace686f9182a44b6c9436f840b0c02f155db
Andrey Voronin <AVor0n@yandex.ru>
2022-05-08 21:41:24 +0300

    feat: add handlers

---
+++ js/handlers.js
@@ -0,0 +1,30 @@
+import { changeLanguage } from './render.js';
+
+export function initKeyboardHandlers() {
+    document.addEventListener('keydown', e => {
+        const $pressedKey = document.querySelector(`.key-${e.code}`);
+        $pressedKey.classList.add('pressed');
+
+        if (e.shiftKey && e.altKey && !e.repeat) {
+            changeLanguage();
+        }
+    });
+
+    document.addEventListener('keyup', e => {
+        const $pressedKey = document.querySelector(`.key-${e.code}`);
+        $pressedKey.classList.remove('pressed');
+    });
+}
+
+export function initMouseHandlers() {
+    const $keyboard = document.querySelector('.keyboard');
+    $keyboard.addEventListener('mousedown', e => {
+        if (e.target.classList.contains('key')) {
+            e.target.classList.add('pressed');
+        }
+    });
+
+    $keyboard.addEventListener('mouseup', () => {
+        document.querySelectorAll('.key').forEach($key => $key.classList.remove('pressed'));
+    });
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
 import { initRender } from './render.js';
+import { initKeyboardHandlers, initMouseHandlers } from './handlers.js';
+
 initRender();
+initKeyboardHandlers();
+initMouseHandlers();
